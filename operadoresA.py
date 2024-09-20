42eb093f03d0318e6c5dcf93cff8819f28b945ec
jirehcorrea <143743324+jirehcorrea@users.noreply.github.com>
2024-09-20 09:39:17 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,48 @@
+print("act 9 clases v2")
+print("eduardo jireh correa garcia:21308051280344")
+#ZONA DE CLASES
+class operadores0344:
+    #ZONA DE FUNCIONES
+    def suma0344(self,E,J):
+        S0344=E+J
+        print(f"la suma de {E} + {J} = {S0344}")
+        
+    def resta0344(self,E,J):
+        S0344=E-J
+        print(f"la resta de {E} - {J} = {S0344}")
+
+    def multiplicacion0344(self,E,J):
+        S0344=E*J
+        print(f"la multiplicacion de {E} * {J} = {S0344}")
+
+    def divi0344(self,E,J):
+        S0344=E/J
+        print(f"la division de {E} / {J} = {S0344}")
+
+    def modulo0344(self,E,J):
+        S0344=E%J
+        print(f"la modulo de {E} % {J} = {S0344}")
+
+    def exponente0344(self,E,J):
+        S0344=E**J
+        print(f"la exponente de {E} ** {J} = {S0344}")
+
+    def cociente0344(self,E,J):
+        S0344=E//J
+        print(f"la cociente de {E} // {J} = {S0344}")
+        #CREACION DE OBJETOS 
+operacorrea=operadores0344()
+#ZONA DE USO DE OBJETOS
+print("funcion suma")
+operacorrea.suma0344(3,2)
+print("funcion resta")
+operacorrea.resta0344(5,2)
+print("funcion multiplicacion")
+operacorrea.multiplicacion0344(5,2)
+print("funcion divicion")
+operacorrea.divi0344(10,2)
+print("funcion modulo")
+operacorrea.modulo0344(5,4)
+print("funcion cociente")
+operacorrea.cociente0344(5,4)
+        
